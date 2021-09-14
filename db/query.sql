78a51a80fbcdf9c712914e59013e8e825de52033
unknown <uekemike@yahoo.com>
2021-09-14 00:19:49 -0400

    create draft query.sql

---
+++ db/query.sql
@@ -0,0 +1,23 @@
+/* 
+view employees
+ Select * from employees
+
+---------------------
+view role
+select * from role
+
+----------------------
+ insert /add department
+insert into department()
+values()
+
+----------------------
+insert /add employee
+insert into employee
+values
+
+----------------------
+ update employee 
+    set title = 
+    where id = 
+*/
